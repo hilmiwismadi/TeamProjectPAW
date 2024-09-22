e339a11912cdd170300cc9fdb879399d35a6636b
fahrinulyanisrina <fahrinulyanisrina@gmail.com>
2024-09-22 17:10:27 +0700

    update model for dosen

--- src/models/dosenModel.js
+++ src/models/dosenModel.js
@@ -1,18 +1,22 @@
 const mongoose = require('mongoose');
 const dosenSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
+        name: {
+            type: String,
+            required: true
+        },
+        username: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        },  
     },
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
+    {
+        timestamps: true,
     }
-});
+);
 
-module.exports = mongoose.model('dosen', dosenSchema);
+    module.exports = mongoose.model('dosen', dosenSchema);
